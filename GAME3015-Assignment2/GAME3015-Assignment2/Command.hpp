2d724df74506a1f98a6042b00a3e9c043b459b07
Michael Denkovski <michaeldenkovski@hotmail.com>
2021-03-06 12:08:16 -0500

    fixed compile error

--- GAME3015-Assignment2/GAME3015-Assignment2/Command.hpp
+++ GAME3015-Assignment2/GAME3015-Assignment2/Command.hpp
@@ -21,12 +21,12 @@ struct Command
 template <typename GameObject, typename Function>
 std::function<void(SceneNode&, const GameTimer&)> derivedAction(Function fn)
 {
-	return [=](SceneNode& node, GameTimer& dt) //cannot convert from 'derived Action::<lambda ae70999ce770862bd1b8049a4852b029>' to 'std::function<void (SceneNode &,const GameTimer &)>
+	return [=](SceneNode& node, const GameTimer& gt) //cannot convert from 'derived Action::<lambda ae70999ce770862bd1b8049a4852b029>' to 'std::function<void (SceneNode &,const GameTimer &)>
 	{
 		// Check if cast is safe
 		assert(dynamic_cast<GameObject*>(&node) != nullptr);
 
 		// Downcast node and invoke function on it
-		fn(static_cast<GameObject&>(node), dt);
+		fn(static_cast<GameObject&>(node), gt);
 	};
 }
